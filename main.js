a2dc1f4e37601417ebdb02fec35c616dff5c9bb5
SanoberShahid <160754505+SanoberShahid@users.noreply.github.com>
2024-02-26 07:19:09 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var variable = "hellow world";
+console.log(variable);
+var message = "hellow words";
+console.log(message); //syntax
+//explicit
+variable = "Another string";
+console.log(variable);
+var num = 1;
+console.log(num);
+var string = "any string";
+console.log(string);
+//implicit
+var variableB = "some string";
+console.log(variableB);
+variable = "sanober";
+console.log(variable);
+variable = "2"; //variable name can contain digit
+console.log(variable);
+variable = "2sanober";
+//json objects:
+var details = {
+    "name": "sanober",
+    "age": 20
+};
+console.log("my name is:" + details.name);
+console.log("my age is:" + details.age);
---
+++ main.ts
@@ -0,0 +1,30 @@
+let variable : string = "hellow world"
+
+console.log(variable);
+let message = "hellow words";
+console.log(message);//syntax
+//explicit
+variable = "Another string";
+console.log(variable);
+let num = 1;
+console.log(num);
+var  string = "any string";
+console.log(string);
+
+//implicit
+let variableB = "some string";
+console.log(variableB);
+variable = "sanober";
+console.log(variable);
+variable = "2";//variable name can contain digit
+console.log(variable);
+
+variable = "2sanober";
+
+//json objects:
+let details = {
+    "name" : "sanober",
+    "age" : 20
+}
+console.log("my name is:" + details.name);
+console.log("my age is:" + details.age);
